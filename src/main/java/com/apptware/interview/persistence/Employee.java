41ccf7c92e52d8fda1f66a6b411efd0a14c4dea3
aniketsabaleapptware <aniket.sabale@apptware.com>
2024-05-03 15:34:03 +0530

    persistence changes

---
+++ src/main/java/com/apptware/interview/persistence/Employee.java
@@ -0,0 +1,17 @@
+package com.apptware.interview.persistence;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+  private UUID id;
+  private String name;
+}
