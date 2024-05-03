e3cf0ccea0d8cc9e55a4a78fd54f0ee0971adcd7
Ajit Kumar <86512082+ajitkmr520@users.noreply.github.com>
2024-05-03 21:52:29 +0530

    Update Student.java

--- src/main/java/com/apptware/interview/immutability/Student.java
+++ src/main/java/com/apptware/interview/immutability/Student.java
@@ -1,15 +1,34 @@
 /** This class is expected to be immutable. Please make necessary changes. */
 package com.apptware.interview.immutability;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
-public class Student {
-  private String name;
-  private Date dateOfBirth;
-  private List<String> courses;
+public final class Student {
+  private final String name;
+  private final Date dateOfBirth; // Change type to Date
+  private final List<String> courses;
+
+  public Student(String name, Date dateOfBirth, List<String> courses) {
+    this.name = name;
+    this.dateOfBirth = new Date(dateOfBirth.getTime()); // Create a new Date object
+    this.courses = Collections.unmodifiableList(courses);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Date getDateOfBirth() {
+    return new Date(dateOfBirth.getTime()); // Return a new Date object
+  }
+
+  public List<String> getCourses() {
+    return courses;
+  }
 }
