0eee04757cc59253fc5ef5f10aba109eec2036cd
aniketsabaleapptware <aniket.sabale@apptware.com>
2024-05-06 14:25:46 +0530

    comments added to classes

--- src/main/java/com/apptware/interview/jpa/Employee.java
+++ src/main/java/com/apptware/interview/jpa/Employee.java
@@ -1,17 +1,13 @@
+/* This class is expected to be an entity class. Please ensure necessary changes are made related to JPA. */
 package com.apptware.interview.jpa;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class Employee {
 
--- src/test/java/com/apptware/interview/jpa/EmployeeTest.java
+++ src/test/java/com/apptware/interview/jpa/EmployeeTest.java
@@ -8,6 +8,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+/**
+ * This test class has a validation for {@link com.apptware.interview.jpa.Employee}.
+ * This test verifies the save operation for the Employee entity. It creates an Employee object,
+ * saves it using the EmployeeRepository, and then retrieves the stored Employee object to ensure
+ * the integrity of its parameters.
+ *
+ * <p>The candidate is expected **NOT** to modify the test case but the corresponding class for
+ * which the test case is written.
+ */
 @DataJpaTest
 public class EmployeeTest {
   private final EmployeeRepository employeeRepository;
