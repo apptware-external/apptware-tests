e2e850861690d18461cb31e8896e7db289cf4317
nikhilpounikar <tacktile@tacktile-System-Product-Name>
2024-06-12 12:04:07 +0530

    test cases update

--- src/main/java/com/apptware/interview/immutability/Student.java
+++ src/main/java/com/apptware/interview/immutability/Student.java
@@ -1,6 +1,8 @@
 /** This class is expected to be immutable. Please make necessary changes. */
 package com.apptware.interview.immutability;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -9,7 +11,21 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Student {
-  private String name;
-  private Date dateOfBirth;
-  private List<String> courses;
+	private String name;
+	private Date dateOfBirth;
+	private List<String> courses;
+
+	// Methods to access fields
+
+	public String getName() {
+		return name;
+	}
+
+	public Date getDateOfBirth() {
+		return new Date(dateOfBirth.getTime()); // Defensive copying to prevent mutation
+	}
+
+	public List<String> getCourses() {
+		return new ArrayList<>(this.courses);
+	}
 }
