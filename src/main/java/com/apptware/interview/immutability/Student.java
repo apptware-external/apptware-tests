b39ba8981df18c4dbdd8f3f489736fa1a0339868
aditya kashyap <adityakashyap2022@gmail.com>
2024-07-19 04:30:51 +0530

    test cases changes

--- src/main/java/com/apptware/interview/immutability/Student.java
+++ src/main/java/com/apptware/interview/immutability/Student.java
@@ -1,15 +1,46 @@
 /** This class is expected to be immutable. Please make necessary changes. */
 package com.apptware.interview.immutability;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
+//@Getter
+//@AllArgsConstructor
 public class Student {
   private String name;
   private Date dateOfBirth;
   private List<String> courses;
+
+  public Student(String name, Date dateOfBirth, List<String> courses) {
+    this.name = name;
+    this.dateOfBirth = new Date(dateOfBirth.getTime());
+    this.courses = new ArrayList<>(courses);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Date getDateOfBirth() {
+    return new Date(dateOfBirth.getTime());
+  }
+
+  public void setDateOfBirth(Date dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public List<String> getCourses() {
+    return new ArrayList<>(courses);
+  }
+
+  public void setCourses(List<String> courses) {
+    this.courses = courses;
+  }
 }
--- src/main/java/com/apptware/interview/jpa/employee/Employee.java
+++ src/main/java/com/apptware/interview/jpa/employee/Employee.java
@@ -2,14 +2,33 @@ package com.apptware.interview.jpa.employee;
 
 import jakarta.persistence.Entity;
 import java.util.UUID;
+
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+//@Getter
+//@Setter
 @Entity
 class Employee {
 
+  @Id
   private UUID id;
   private String name;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
