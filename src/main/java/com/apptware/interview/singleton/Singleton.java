2b4479f25bd7b1c7933ce5c54648d4b4643a0938
pranav-kharche <pranavakharche1@gmail.com>
2024-08-01 13:43:37 +0530

    Added Singletone class

--- src/main/java/com/apptware/interview/singleton/Singleton.java
+++ src/main/java/com/apptware/interview/singleton/Singleton.java
@@ -2,17 +2,43 @@
 package com.apptware.interview.singleton;
 
 public class Singleton {
-  private static Singleton single_instance = null;
+
+  //volatile - changes to instance visible to all threads
+  private static volatile Singleton instance = null;
 
   public String s;
+
 
   private Singleton() {
     s = "Hello I am a string part of Singleton class";
   }
 
-  public static synchronized Singleton getInstance() {
-    if (single_instance == null) single_instance = new Singleton();
+  //provides global access to singletone instance
+  //apply double-checked locking mechanism
+  public static Singleton getInstance() {
+    if (instance == null) {
+      synchronized (Singleton.class) {
+        if (instance == null) {
+          instance = new Singleton();
+        }
+      }
+    }
+    return instance;
+  }
 
-    return single_instance;
+  //used during deserialization (it returns the existing instance instead of creating a new one.)
+  private Object readResolve() {
+    return getInstance();
+  }
+
+  //for prevent cloning of singletonr instance
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    throw new CloneNotSupportedException("Cannot clone a Singleton");
+  }
+  //based on instance identity, returns hash code of singletone instance
+  @Override
+  public int hashCode() {
+    return System.identityHashCode(instance);
   }
 }
