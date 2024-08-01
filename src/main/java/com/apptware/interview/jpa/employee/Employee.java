c04d46b8a8dd932463a15348525c89ac051b5e6e
pranav-kharche <pranavakharche1@gmail.com>
2024-08-01 13:34:07 +0530

    Added employee class

--- src/main/java/com/apptware/interview/jpa/employee/Employee.java
+++ src/main/java/com/apptware/interview/jpa/employee/Employee.java
@@ -1,15 +1,24 @@
 package com.apptware.interview.jpa.employee;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import java.util.UUID;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 class Employee {
 
-  private UUID id;
+  @Id // for  primary  key
+  private UUID id;  // no default value
   private String name;
+
+  public Employee(String name) {
+    this.name = name;
+    this.id = UUID.randomUUID(); // creating UUId, every employee can unique and identified.
+  }
 }
