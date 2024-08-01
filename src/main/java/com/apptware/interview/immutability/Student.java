057bd70c9338a66bba2f7beb6b93f0cac431922b
pranav-kharche <pranavakharche1@gmail.com>
2024-08-01 13:30:50 +0530

    Added immutability class student

--- src/main/java/com/apptware/interview/immutability/Student.java
+++ src/main/java/com/apptware/interview/immutability/Student.java
@@ -1,15 +1,25 @@
 /** This class is expected to be immutable. Please make necessary changes. */
 package com.apptware.interview.immutability;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class Student {
-  private String name;
-  private Date dateOfBirth;
-  private List<String> courses;
+//for ensure immutability, add custom constructor and overrides the accessor methods
+public record Student(String name, Date dateOfBirth, List<String> courses) {
+  public Student(String name, Date dateOfBirth, List<String> courses) {
+    this.name = name;
+    this.dateOfBirth = new Date(dateOfBirth.getTime());
+    this.courses = new ArrayList<>(courses);
+  }
+
+  @Override
+  public Date dateOfBirth() {
+    return new Date(dateOfBirth.getTime());
+  }
+
+  @Override
+  public List<String> courses() {
+    return new ArrayList<>(courses);
+  }
 }
