0bdf287b4f8dffc052095e565d11dd88784838ea
Ritesh-verma11 <ritesh.verma0837@gmail.com>
2024-08-06 17:31:12 +0530

    Modified Singleton class

--- src/main/java/com/apptware/interview/singleton/Singleton.java
+++ src/main/java/com/apptware/interview/singleton/Singleton.java
@@ -1,35 +1,21 @@
 /** This class is expected to be a singleton. Please make necessary changes. */
 package com.apptware.interview.singleton;
 
-import java.io.Serializable;
-
-public class Singleton implements Serializable {
-  private static volatile Singleton single_instance;
+public class Singleton {
+  private static Singleton single_instance = null;
 
   public String s;
 
   private Singleton() {
     if (single_instance != null) {
+      s = "Hello I am a string part of Singleton class";
       throw new IllegalStateException("Instance already created.");
     }
-    single_instance = this;
-    s = "Hello I am a string part of Singleton class";
   }
 
-  public static Singleton getInstance() {
-    if (single_instance == null) {
-      synchronized (Singleton.class) {
-        if (single_instance == null) {
-          single_instance = new Singleton();
-        }
-      }
-    }
+  public static synchronized Singleton getInstance() {
+    if (single_instance == null) single_instance = new Singleton();
+
     return single_instance;
   }
-
-  // Ensure singleton property during deserialization
-  protected Object readResolve() {
-    return getInstance();
-  }
 }
-
