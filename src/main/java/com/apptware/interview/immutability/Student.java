54a200af8d9398fa1816bcb1e806ad0be193bd8b
Vishal Lokhande <vishalrl@lentra.ai>
2024-08-09 12:47:01 +0530

    1:- Consturctor and getter methods provided for Student Class 2:- @Id annotation was missing foe @Entity Employee 3:-NoUniqueBeanDefinitionException   for this There was 2 options add bean in config or use @Scope I have added bean configuration

--- src/main/java/com/apptware/interview/immutability/Student.java
+++ src/main/java/com/apptware/interview/immutability/Student.java
@@ -1,19 +1,29 @@
 /** This class is expected to be immutable. Please make necessary changes. */
 package com.apptware.interview.immutability;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Student {
   private String name;
   private Date dateOfBirth;
   private List<String> courses;
 
+  public Student(String name, Date dateOfBirth, List<String> courses) {
+    this.name = name;
+    this.dateOfBirth = new Date(dateOfBirth.getTime());
+    this.courses = new ArrayList<>(courses);
+  }
+
   public Date getDateOfBirth() {
     return new Date(dateOfBirth.getTime());
+  }
+
+  public List<String> getCourses() {
+    return new ArrayList<>(courses);
   }
 }
