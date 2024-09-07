3124c3fb5eb4fcc8177f868f3bfd225be5c6b30a
Vaibhavwani11 <vaibhavwani2428@gmail.com>
2024-09-08 00:42:50 +0530

    Fixed the failing test cases.

--- src/main/java/com/apptware/interview/spring/beans/BeanFactory.java
+++ src/main/java/com/apptware/interview/spring/beans/BeanFactory.java
@@ -2,6 +2,7 @@ package com.apptware.interview.spring.beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +11,14 @@ public class BeanFactory {
   @Autowired private ApplicationContext context;
 
   public OnDemand getOnDemandBean(SomeEnum someEnum, String someString) {
-    return context.getBean(BaseOnDemand.class, someString);
+
+      switch (someEnum) {
+        case SOME_ENUM_A:
+          return context.getBean(OnDemandA.class, someString);
+        case SOME_ENUM_B:
+          return context.getBean(OnDemandB.class, someString);
+        default:
+          return context.getBean(BaseOnDemand.class, someString);
+      }
   }
 }
