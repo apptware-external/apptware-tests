24f424683096877ae2621de4271cd2405e206e42
Jayesh Palve <1805jayespalve@gmail.com>
2024-09-18 13:45:37 +0530

    "updated Employee.java & Singleton.java"

--- src/main/java/com/apptware/interview/singleton/Singleton.java
+++ src/main/java/com/apptware/interview/singleton/Singleton.java
@@ -1,19 +1,27 @@
 /** This class is expected to be a singleton. Please make necessary changes. */
 package com.apptware.interview.singleton;
 
+import java.util.Objects;
+
 public class Singleton {
   private static Singleton single_instance = null;
 
   public String s;
 
-  // Throwing an exception is not expected.
   private Singleton() {
     s = "Hello I am a string part of Singleton class";
   }
 
   public static synchronized Singleton getInstance() {
-    if (single_instance == null) single_instance = new Singleton();
+		if (single_instance == null) {
+			single_instance = new Singleton();
+		}
 
     return single_instance;
   }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(s);
+	}
 }
