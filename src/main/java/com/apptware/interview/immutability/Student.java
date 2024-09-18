1996c347fed7043fb3d008fa63e2481c7c12bb3b
Jayesh Palve <1805jayespalve@gmail.com>
2024-09-18 14:22:39 +0530

    updated "Student.java & It's Test Class"

---
+++ src/main/java/com/apptware/interview/immutability/Student.java
@@ -0,0 +1,24 @@
+/** This class is expected to be immutable. Please make necessary changes. */
+package com.apptware.interview.immutability;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public final class Student {
+	private final String name;
+	private final Date dateOfBirth;
+	private final List<String> courses;
+
+	public Date getDateOfBirth() {
+		return new Date(dateOfBirth.getTime());
+	}
+
+	public List<String> getCourses() {
+		return new ArrayList<>(courses);
+	}
+}
